b106358b24328bc084df5da81b0cc9f208f44f0f
Maycon de Moraes <mayconmoraes.dm@gmail.com>
2020-04-08 18:22:23 -0300

    first commit

---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,36 @@
+package launch;
+
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import javax.servlet.ServletException;
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) throws ServletException, LifecycleException {
+        String webappDirLocation = "src/main/";
+        Tomcat tomcat = new Tomcat();
+
+        Integer webPort = 8080;
+        tomcat.setPort(webPort);
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuração da aplicação no dir:  " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declaração do WEB-INF
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+    }
+}
